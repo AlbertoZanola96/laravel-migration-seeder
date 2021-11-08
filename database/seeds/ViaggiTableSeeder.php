312668a3e19edc08764580aa09120b7b4b13f1f8
AlbertoZanola <albertozanola1996@gmail.com>
2021-11-08 15:41:34 +0100

    Completamento esercizio

--- database/seeds/ViaggiTableSeeder.php
+++ database/seeds/ViaggiTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Viaggio;
+use Faker\Generator as Faker;
 
 class ViaggiTableSeeder extends Seeder
 {
@@ -10,15 +11,17 @@ class ViaggiTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        $new_viaggio = new Viaggio();
-        $new_viaggio->city= 'Milano';
-        $new_viaggio->state = 'Italia';
-        $new_viaggio->address = 'Via Montenapoleone';
-        $new_viaggio->night = 3;
-        $new_viaggio->description = 'text';
-        $new_viaggio->price = 120;
-        $new_viaggio->save();
+        for ($i = 0; $i < 100; $i++) {
+            $new_viaggio = new Viaggio();
+            $new_viaggio->city= $faker->city();
+            $new_viaggio->state = $faker->state();
+            $new_viaggio->address = $faker->streetAddress();
+            $new_viaggio->night = $faker->numberBetween(1, 30);
+            $new_viaggio->description = $faker->paragraphs(5, true);
+            $new_viaggio->price = $faker->numberBetween(20, 5000);
+            $new_viaggio->save();
+        }
     }
 }
